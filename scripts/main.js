f421d7bbb631c91facb8a7ff0b3345b21eb33fa1
Wesley <DevWesleyMedeiros@yahoo.com>
2024-09-18 17:26:20 -0300

    alteração no nome de arquivos e na inserção do .gitgnore

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+import 'bootstrap';
+import $ from 'jquery';
+import '../src/style.scss'
+
+
+$(document).ready(()=>{
+    alert("Estou fucionando!!");
+})
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx,html,scss}",
+    "./scripts/**/*.{js,jsx,ts,tsx,html,scss}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
