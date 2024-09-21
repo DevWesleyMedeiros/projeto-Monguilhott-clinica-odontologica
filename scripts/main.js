448a345772063cf969ae8dd82712cdcd0c6d3a7e
Wesley <DevWesleyMedeiros@yahoo.com>
2024-09-20 22:49:05 -0300

    Header concluído

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,21 @@ import 'bootstrap';
 import $ from 'jquery';
 import '../src/style.scss'
 
-
-$(document).ready(()=>{
     
-})
+$(document).ready(function() {
+    let arrayHeaderItems = $('nav > ul > li');
+    
+    $.each(arrayHeaderItems, function(indexInArray, valueOfElement) {
+            
+        $(valueOfElement).hover(
+            function() { // Mouse in
+                let divSelected = $('<div></div>').addClass('selected');
+                $(this).append(divSelected);
+            },
+            function() { // Mouse out
+                $(this).find('.selected').remove();
+            }
+        );
+    });
+});
+
